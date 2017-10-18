116bd8d3f8c6ff52ecbd17ae148fe4611385d130
Reshad007 <reshad_aziz@yahoo.com>
2017-10-18 02:02:49 -0700

    isValidData - working stage

--- notmyjs.js
+++ notmyjs.js
@@ -62,117 +62,117 @@ $(document).ready( function() {
             elementHandle[1].focus();            
             return false;
             }
-        if(isEmpty(elementHandle[2].val())) {
-            elementHandle[2].addClass("error");
-            errorStatusHandle.text("Please enter your address");
-            elementHandle[2].focus();            
-            return false;
-            }
         if(isEmpty(elementHandle[3].val())) {
             elementHandle[3].addClass("error");
-            errorStatusHandle.text("Please enter your city");
+            errorStatusHandle.text("Please enter your address");
             elementHandle[3].focus();            
             return false;
             }
         if(isEmpty(elementHandle[4].val())) {
             elementHandle[4].addClass("error");
-            errorStatusHandle.text("Please enter your state");
-            elementHandle[4].focus();            
-            return false;
-            }
-        if(!isValidState(elementHandle[4].val())) {
-            elementHandle[4].addClass("error");
-            errorStatusHandle.text("The state appears to be invalid, "+
-            "please use the two letter state abbreviation");
+            errorStatusHandle.text("Please enter your city");
             elementHandle[4].focus();            
             return false;
             }
         if(isEmpty(elementHandle[5].val())) {
             elementHandle[5].addClass("error");
-            errorStatusHandle.text("Please enter your zip code");
+            errorStatusHandle.text("Please enter your state");
             elementHandle[5].focus();            
             return false;
             }
-        if(!$.isNumeric(elementHandle[5].val())) {
+        if(!isValidState(elementHandle[5].val())) {
             elementHandle[5].addClass("error");
-            errorStatusHandle.text("The zip code appears to be invalid, "+
-            "numbers only please. ");
-            elementHandle[5].focus();            
-            return false;
-            }
-        if(elementHandle[5].val().length != 5) {
-            elementHandle[5].addClass("error");
-            errorStatusHandle.text("The zip code must have exactly five digits")
+            errorStatusHandle.text("The state appears to be invalid, "+
+            "please use the two letter state abbreviation");
             elementHandle[5].focus();            
             return false;
             }
         if(isEmpty(elementHandle[6].val())) {
             elementHandle[6].addClass("error");
-            errorStatusHandle.text("Please enter your area code");
+            errorStatusHandle.text("Please enter your zip code");
             elementHandle[6].focus();            
             return false;
-            }            
+            }
         if(!$.isNumeric(elementHandle[6].val())) {
             elementHandle[6].addClass("error");
-            errorStatusHandle.text("The area code appears to be invalid, "+
+            errorStatusHandle.text("The zip code appears to be invalid, "+
             "numbers only please. ");
             elementHandle[6].focus();            
             return false;
             }
-        if(elementHandle[6].val().length != 3) {
+        if(elementHandle[6].val().length !=5) {
             elementHandle[6].addClass("error");
-            errorStatusHandle.text("The area code must have exactly three digits")
+            errorStatusHandle.text("The zip code must have exactly five digits")
             elementHandle[6].focus();            
             return false;
-            }   
+            }
         if(isEmpty(elementHandle[7].val())) {
             elementHandle[7].addClass("error");
-            errorStatusHandle.text("Please enter your phone number prefix");
+            errorStatusHandle.text("Please enter your area code");
             elementHandle[7].focus();            
             return false;
-            }           
+            }            
         if(!$.isNumeric(elementHandle[7].val())) {
             elementHandle[7].addClass("error");
-            errorStatusHandle.text("The phone number prefix appears to be invalid, "+
+            errorStatusHandle.text("The area code appears to be invalid, "+
             "numbers only please. ");
             elementHandle[7].focus();            
             return false;
             }
         if(elementHandle[7].val().length != 3) {
             elementHandle[7].addClass("error");
-            errorStatusHandle.text("The phone number prefix must have exactly three digits")
+            errorStatusHandle.text("The area code must have exactly three digits")
             elementHandle[7].focus();            
             return false;
-            }
+            }   
         if(isEmpty(elementHandle[8].val())) {
             elementHandle[8].addClass("error");
-            errorStatusHandle.text("Please enter your phone number");
+            errorStatusHandle.text("Please enter your phone number prefix");
             elementHandle[8].focus();            
             return false;
-            }            
+            }           
         if(!$.isNumeric(elementHandle[8].val())) {
             elementHandle[8].addClass("error");
-            errorStatusHandle.text("The phone number appears to be invalid, "+
+            errorStatusHandle.text("The phone number prefix appears to be invalid, "+
             "numbers only please. ");
             elementHandle[8].focus();            
             return false;
             }
-        if(elementHandle[8].val().length != 4) {
+        if(elementHandle[8].val().length != 3) {
             elementHandle[8].addClass("error");
-            errorStatusHandle.text("The phone number must have exactly four digits")
+            errorStatusHandle.text("The phone number prefix must have exactly three digits")
             elementHandle[8].focus();            
             return false;
-            }  
+            }
         if(isEmpty(elementHandle[9].val())) {
             elementHandle[9].addClass("error");
-            errorStatusHandle.text("Please enter your email address");
+            errorStatusHandle.text("Please enter your phone number");
             elementHandle[9].focus();            
             return false;
             }            
-        if(!isValidEmail(elementHandle[9].val())) {
+        if(!$.isNumeric(elementHandle[9].val())) {
             elementHandle[9].addClass("error");
-            errorStatusHandle.text("The email address appears to be invalid,");
+            errorStatusHandle.text("The phone number appears to be invalid, "+
+            "numbers only please. ");
             elementHandle[9].focus();            
+            return false;
+            }
+        if(elementHandle[9].val().length != 4) {
+            elementHandle[9].addClass("error");
+            errorStatusHandle.text("The phone number must have exactly four digits")
+            elementHandle[9].focus();            
+            return false;
+            }  
+        if(isEmpty(elementHandle[10].val())) {
+            elementHandle[10].addClass("error");
+            errorStatusHandle.text("Please enter your email address");
+            elementHandle[10].focus();            
+            return false;
+            }            
+        if(!isValidEmail(elementHandle[10].val())) {
+            elementHandle[10].addClass("error");
+            errorStatusHandle.text("The email address appears to be invalid,");
+            elementHandle[10].focus();            
             return false;
             }                                                                       
         return true;
@@ -192,40 +192,40 @@ $(document).ready( function() {
         errorStatusHandle.text("");
         });
         
-    elementHandle[9].on('blur', function() {
-        if(isEmpty(elementHandle[9].val()))
+    elementHandle[10].on('blur', function() {
+        if(isEmpty(elementHandle[10].val()))
             return;
-        if(isValidEmail(elementHandle[9].val())) {
+        if(isValidEmail(elementHandle[10].val())) {
             $(this).removeClass("error");
             errorStatusHandle.text("");
             }
         });        
 /////////////////////////////////////////////////////////////////        
 
-    elementHandle[4].on('keyup', function() {
-        elementHandle[4].val(elementHandle[4].val().toUpperCase());
+    elementHandle[5].on('keyup', function() {
+        elementHandle[5].val(elementHandle[5].val().toUpperCase());
         });
         
-    elementHandle[6].on('keyup', function() {
-        if(elementHandle[6].val().length == 3)
-            elementHandle[7].focus();
-            });
-            
     elementHandle[7].on('keyup', function() {
         if(elementHandle[7].val().length == 3)
             elementHandle[8].focus();
+            });
+            
+    elementHandle[8].on('keyup', function() {
+        if(elementHandle[8].val().length == 3)
+            elementHandle[9].focus();
             });            
 
    
     $(':submit').on('click', function() {
-        for(var i=0; i < 10; i++)
+        for(var i=0; i < 11; i++)
             elementHandle[i].removeClass("error");
         errorStatusHandle.text("");
         return isValidData();
         });
         
     $(':reset').on('click', function() {
-        for(var i=0; i < 10; i++)
+        for(var i=0; i < 11; i++)
             elementHandle[i].removeClass("error");
         errorStatusHandle.text("");
         });                                       
